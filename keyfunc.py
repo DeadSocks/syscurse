1f5e490644b67121cd65e5b242de1e418845d9dc
DeadSocks <ocdotson@wpi.edu>
2015-08-18 10:38:45 -0400

    Added more documentation

--- keyfunc.py
+++ keyfunc.py
@@ -37,15 +37,15 @@ def str_trackpad(trackpadOff):
 ##############
 #Actual key function
 ##############
-def inc_vol():
+def inc_vol():#Increments the master volume by 5%
 	curses.endwin()
 	execute_cmd("amixer -D pulse sset Master 5%+")
 	
-def dec_vol():
+def dec_vol():#Decrements the master volume by 5%
 	curses.endwin()
 	execute_cmd("amixer -D pulse sset Master 5%-")
 	
-def toggle_trackpad(trackpadOff):
+def toggle_trackpad(trackpadOff):#Toggles the trackpad between on and off. Currently does not do so intelligently, assumes it is on and keeps track of toggling rather than sensing the current state.
 	curses.endwin()
 	if(trackpadOff == 0):
 		execute_cmd("synclient TouchpadOff=1")
@@ -53,14 +53,13 @@ def toggle_trackpad(trackpadOff):
 	else:
 		execute_cmd("synclient TouchpadOff=0")
 		return 0
-def disable_VGA():
+def disable_VGA():#Turns off VGA connection 
 	curses.endwin()
 	execute_cmd("xrandr --output VGA-1-2 --off")
 
-def open_firefox():
+def open_firefox():#Open firefox 
 	execute_cmd("firefox")
-def open_chrome():
+def open_chrome():#Doesn't really work, but it tries to open chrome. 
 	execute_cmd("google-chrome")
-def hibernate():
+def hibernate():#Hibernate the computer? Maybe? Sudo??
 	execute_cmd("sudo pm-hibernate")
-
